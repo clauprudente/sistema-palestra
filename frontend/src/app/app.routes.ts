ef37650ff4b0ede08f6dc4d15929416f87d554af
clauprudente <claudiamary.santos@gmail.com>
2026-06-15 12:01:37 -0300

    feat: add cadastro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+
+    {
+        path: 'cadastro',
+        component: CadastroComponent
+    },
+
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
---
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,65 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from
+  '@angular/forms'
+import { Router } from '@angular/router';
+
+interface respostaCadastro {
+  message: string;
+}
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroComponent {
+  constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef) { }
+
+  formularioCadastro = new FormGroup({
+    email: new FormControl('', Validators.required),
+    nome: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  });
+
+  mensagem: string = '';
+  tipoMensagem: 'success' | 'danger' = 'success';
+
+  onSubmit() {
+    if (this.formularioCadastro.valid) {
+      this.http
+        .post<respostaCadastro>(
+          'http://localhost:3000/api/cadastro',
+          this.formularioCadastro.value
+        )
+        .subscribe({
+          next: (res) => {
+            this.tipoMensagem = 'success';
+            this.mensagem = res.message;
+            this.cdr.detectChanges();
+
+            setTimeout(() => {
+              this.router.navigate(['/login']);
+            }, 2000);
+          },
+          error: (err) => {
+            this.tipoMensagem = 'danger';
+            if (err?.status === 400) {
+              this.mensagem = err?.error?.message || 'Email já cadastrado';
+            } else {
+              this.mensagem = err?.error?.message || 'Erro ao cadastrar';
+            }
+            this.cdr.detectChanges();
+          },
+        });
+    }
+  }
+}
